8db61ed805e1c1ae87dde888803da3ab2baa90cf
xmskyahya-cpu <xmskyahya@gmail.com>
2026-04-16 23:33:36 +0100

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -15,7 +15,7 @@ global.setting = {
 
 global.info = {
  nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ pairingNumber : '212726516587',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
